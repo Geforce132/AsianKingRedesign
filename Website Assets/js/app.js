95e6b7ca79ac328d79ced398fe3a093c45f83bcf
Geforce <Geforce132@gmail.com>
2020-04-21 19:21:01 -0500

    Added menu component

--- Website Assets/js/app.js
+++ Website Assets/js/app.js
@@ -1,8 +1,10 @@
 $(function(){
-  let menuToggler     = $("nav button.navbar-toggler");
+  let navMenuToggler  = $("nav button.navbar-toggler");
   let dropdownToggler = $("a.nav-link.dropdown-toggle");
+  let menuToggler     = $(".menu-nav ul li");
+  let menuContentArea = $("main .menu-content-area");
 
-  menuToggler.on("click", function(e){
+  navMenuToggler.on("click", function(e){
     e.preventDefault();
     let navigation = $(this).attr("data-target");
     $(navigation).toggleClass("open");
@@ -12,4 +14,27 @@ $(function(){
     e.preventDefault();
     $(this).siblings().toggleClass("open");
   });
-})
+
+  menuToggler.on("click", function(e){
+    e.preventDefault();
+    $(this).toggleClass("category-selected");
+    $(this).siblings().removeClass("category-selected");
+
+    // get a reference to the food category list
+    var ulList = document.getElementById("menu-nav");
+
+    // save which category was clicked on
+    var liClicked = e.target.closest("li");
+
+    // get the list of all the categories from the main food category list
+    var listOfElements = Array.from(ulList.children);
+
+    // get the index of the category which was clicked
+    var index = listOfElements.indexOf(liClicked);
+
+    // remove the category-selected class from the previously selected category
+    // and add it to the category which was clicked on 
+    $(menuContentArea).children().removeClass("category-selected");
+    $(menuContentArea).children()[index].classList.add("category-selected");
+  });
+});
